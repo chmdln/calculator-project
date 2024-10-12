a7597a1cb93b3bff24a24d0c5fa3ca44ca3ab3e7
tatyana <beatytanya@gmail.com>
2024-10-12 12:52:01 -0400

    Fix overflow issues in display container

--- calculator.js
+++ calculator.js
@@ -4,23 +4,27 @@ let second = [];
 let operator;
 let res; 
 
+
 let displayContainerElem = document.querySelector(".display-container"); 
 let displayElem = document.querySelector(".js-display-text"); 
 
 document.querySelectorAll(".js-digit-button")
     .forEach((elem) => {
         elem.addEventListener("click", () => {
+            // handle leading zero
+            if (elem.innerText === "0" && temp.length === 0) {
+                return 
+            }
             temp.push(elem.innerText); 
             displayElem.innerText = temp.join("");
-            if (displayElem.scrollWidth > displayContainerElem.clientWidth-20) {
-                scaleTextToFit();
-            } else {
-                displayElem.style.fontSize = '50px'; 
-            }
+            console.log(displayElem.scrollWidth)
 
-            console.log(elem.innerText); 
+            scaleTextToFit(); 
+
+            // console.log(elem.innerText); 
         })
     })
+
 
 
 document.querySelectorAll(".js-operator-button")
@@ -30,6 +34,12 @@ document.querySelectorAll(".js-operator-button")
                 first = [...temp]; 
                 temp = [];  
             } else if (second.length === 0) {
+                // dont' do anything, if "=" operator 
+                // was pressed earlier and temp is empty
+                if (temp.length === 0) {
+                    operator = elem.id 
+                    return 
+                }
                 second = [...temp]; 
                 res = operate(first, second, operator);
                 first = [res];  
@@ -37,22 +47,16 @@ document.querySelectorAll(".js-operator-button")
                 temp = [];  
 
                 displayElem.innerText = res; 
-                console.log(displayElem.scrollWidth)
-                if (displayElem.scrollWidth > displayContainerElem.clientWidth-20) {
-                    scaleTextToFit();
-                } else {
-                    displayElem.style.fontSize = '50px'; 
-                }
-                   
+                scaleTextToFit(); 
             }
             
             // assign the operator 
             operator = elem.id;
 
-            console.log(first); 
-            console.log(second); 
-            console.log(temp); 
-            console.log(operator)
+            // console.log(first); 
+            // console.log(second); 
+            // console.log(temp); 
+            // console.log(operator)
             
         })
     })
@@ -67,21 +71,18 @@ document.getElementById("=").addEventListener('click', (elem) => {
 
     displayElem.innerText = res; 
     console.log(displayElem.scrollWidth)
-    if (displayElem.scrollWidth > displayContainerElem.clientWidth-20) {
-        scaleTextToFit();
-    } else {
-        displayElem.style.fontSize = '50px'; 
-    }
+
+    scaleTextToFit(); 
 })
 
 
-document.querySelector(".js-ac-button").addEventListener('click', clear); 
+document.querySelector(".js-ac-button").addEventListener('click', _clear); 
 
 
 function operate(arr1, arr2, op) {
+    let output;
     let val1 = Number(arr1.join("")); 
-    let val2 = Number(arr2.join("")); 
-    let output; 
+    let val2 = Number(arr2.join(""));  
     
     if (op === "+") {
         output = val1 + val2
@@ -92,29 +93,50 @@ function operate(arr1, arr2, op) {
     } else if (op === "*") {
         output = val1 * val2
     } 
-    return output
+    return checkAndTruncate(output)
+}
+
+
+function checkAndTruncate(res) {
+    let temp = res.toString(); 
+    let num1; 
+    let num2; 
+
+    if (temp.includes("e")) {
+        index = temp.indexOf("e"); 
+        num1 = temp.slice(0, 4); 
+        num2 = temp.slice(index-1, temp.length); 
+        return Number(num1+num2) 
+    }
+    return res 
 }
 
 
 function scaleTextToFit() {
-    let fontSize = 100; 
-    displayElem.computedStyleMap.fontSize = fontSize + 'px'; 
+    let currFontSize = 50; 
+    displayElem.style.fontSize = currFontSize + 'px';
 
     while (displayElem.scrollWidth > (displayContainerElem.clientWidth-20)) {
-        fontSize--; 
-        displayElem.style.fontSize = fontSize + 'px'; 
+        currFontSize--; 
+        displayElem.style.fontSize = currFontSize + 'px'; 
     }
+
+    // while (displayElem.scrollWidth < (displayContainerElem.clientWidth-20)) {
+    //     currFontSize++; 
+    //     displayElem.style.fontSize = currFontSize + 'px'; 
+    // }
 }
 
-function clear() {
+function _clear() {
     temp = []; 
     first = []; 
     second = []; 
-    operator;
+    operator = "";
     res = "";  
 
     displayElem.innerText = 0; 
     displayElem.style.fontSize = '50px'; 
+
 }
 
 
